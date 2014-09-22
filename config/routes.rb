3e035931a2867f8a26bdea5505df67540752eaec
trevmckendrick <trevormckendrick@gmail.com>
2014-09-22 13:17:34 -0600

    Add jobs page

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
   get 'request-phone-call' => 'pages#request_phone_call'
   get 'request-invite' => 'pages#request_invite'
   get 'example-tasks' => 'pages#example_tasks'
+  get 'jobs' => 'pages#jobs'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

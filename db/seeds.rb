96598c82313345926d2609bd83f8b6b88f82b3e0
trevmckendrick <trevormckendrick@gmail.com>
2014-10-27 21:19:56 -0600

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+# Customer.create(:first_name => "Chris", :stripe_id => "cus_4zmXWB5PAcNuka", :company_id => "1")
+# Customer.create(:first_name => "Breanna", :stripe_id => "cus_4zRPzYLtHgk8tF", :company_id => "2")
+# Customer.create(:first_name => "Barbara", :stripe_id => "cus_51CjOslwidbann", :company_id => "3")
+#
+# Company.create(:name => "SuperMegaUltraGroovy", :id => "1")
+# Company.create(:name => "Breanna McKendrick Photography", :id => "2")
+# Company.create(:name => "RobinsonBurnett and Associates", :id => "3")

cca618ab9598e39a9257b6ba072165ed37ca4de2
trevmckendrick <trevormckendrick@gmail.com>
2014-10-27 21:21:20 -0600

    Migrate StripeId column from customers to companies

---
+++ db/migrate/20141028030920_move_stripe_id_column_to_companies.rb
@@ -0,0 +1,11 @@
+class MoveStripeIdColumnToCompanies < ActiveRecord::Migration
+  def change
+    add_column :companies, :stripe_id, :string
+
+    Customer.find_each do |customer|
+      customer.company.stripe_id = customer.stripe_id
+    end
+
+    remove_column :customers, :stripe_id
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141028025655) do
+ActiveRecord::Schema.define(version: 20141028030920) do
 
   create_table "companies", force: true do |t|
     t.string   "name"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20141028025655) do
     t.string   "main_phone"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "stripe_id"
   end
 
   create_table "customers", force: true do |t|
@@ -32,7 +33,6 @@ ActiveRecord::Schema.define(version: 20141028025655) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.string   "stripe_id"
     t.integer  "company_id"
   end
 

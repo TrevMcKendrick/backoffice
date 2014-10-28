77e5d385af912e13ba8153379a496da05015fd4d
trevmckendrick <trevormckendrick@gmail.com>
2014-10-27 22:30:08 -0600

    miracle?

--- db/migrate/20141028030920_move_stripe_id_column_to_companies.rb
+++ db/migrate/20141028030920_move_stripe_id_column_to_companies.rb
@@ -1,5 +1,8 @@
 class MoveStripeIdColumnToCompanies < ActiveRecord::Migration
   def change
+    Customer.reset_column_information
+    Company.reset_column_information
+
     Customer.all.each do |customer|
       customer.company.stripe_id = customer.stripe_id
     end

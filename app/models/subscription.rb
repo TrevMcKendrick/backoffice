ac8d88f449e7503ce9e48730fec3c8d4cd2a5a0b
trevmckendrick <trevormckendrick@gmail.com>
2014-11-13 16:39:19 -0700

    Retrieve Stripe subscription data from Subscription class

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,14 +1,18 @@
 class Subscription < ActiveRecord::Base
   belongs_to :company
 
-  def current_period_end
-
-  end
-
   def current_period_start
+    stripe_subscription.current_period_start
   end
 
-  def stripe_subscription
-    # self.company
+  def current_period_end
+    stripe_subscription.current_period_end
   end
+
+  private
+  
+  def stripe_subscription
+    self.company.stripe_customer_object.subscriptions.data.first
+  end
+
 end

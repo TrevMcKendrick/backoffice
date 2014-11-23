2eb1c905c5221d953b9648853c497954558964be
trevmckendrick <trevormckendrick@gmail.com>
2014-11-23 15:31:45 -0700

    Add size method to subscription

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -9,8 +9,16 @@ class Subscription < ActiveRecord::Base
     stripe_subscription.current_period_end
   end
 
+  def size
+    self.company.stripe_customer_object.subscriptions.data.first.plan[:id][0,2].to_i
+  end
+
+  def hours_remaining
+    
+  end
+
   private
-  
+
   def stripe_subscription
     self.company.stripe_customer_object.subscriptions.data.first
   end

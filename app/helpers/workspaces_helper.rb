88a75017e34983bd8315e91f6281b421a356680b
trevmckendrick <trevormckendrick@gmail.com>
2014-11-24 00:22:34 -0700

    Added UNIX timestamp to Toggle time converted to workspace helper

--- app/helpers/workspaces_helper.rb
+++ app/helpers/workspaces_helper.rb
@@ -1,7 +1,9 @@
 module WorkspacesHelper
-
-  def readable_date(timpestamp)
-    Time.at(timpestamp).strftime("%B %-d, %Y")
+  def readable_date(timestamp)
+    Time.at(timestamp).strftime("%B %-d, %Y")
   end
 
+  def unix_to_toggl(timestamp)
+    Time.at(timestamp).strftime("%Y-%m-%d")
+  end
 end

ebd631298d73c55bd5dad4dec15e08434ae2659a
trevmckendrick <trevormckendrick@gmail.com>
2014-11-24 00:23:45 -0700

    add hours_used method

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -13,14 +13,23 @@ class Subscription < ActiveRecord::Base
     self.company.stripe_customer_object.subscriptions.data.first.plan[:id][0,2].to_i
   end
 
+  def hours_used(since, ended)
+    ms = TogglRequester.new(:client_name => self.company.name, :since => since, :until => ended).time
+    ms == nil ? 0 : milliseconds_to_hours(ms) 
+  end
+
   def hours_remaining
-    
+    size - hours_used
   end
 
   private
 
   def stripe_subscription
     self.company.stripe_customer_object.subscriptions.data.first
+  end
+
+  def milliseconds_to_hours(ms)
+    ms / 3600000
   end
 
 end

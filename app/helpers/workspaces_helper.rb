76f0c8f65c59b169c1035108088ec908513df852
trevmckendrick <trevormckendrick@gmail.com>
2014-11-30 04:43:57 -0700

    moved ms formatters to helper

--- app/helpers/workspaces_helper.rb
+++ app/helpers/workspaces_helper.rb
@@ -6,4 +6,12 @@ module WorkspacesHelper
   def unix_to_toggl(timestamp)
     Time.at(timestamp).strftime("%Y-%m-%d")
   end
+
+  def ms_to_hours(ms)
+    ms == nil ? 0 : (ms / 3600000) % 24
+  end
+
+  def ms_to_minutes(ms)
+  	ms == nil ? 0 : (ms / 60000) % 60
+  end
 end

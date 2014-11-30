83aca3f22500ef55ff9f274c7b72908689a9376e
trevmckendrick <trevormckendrick@gmail.com>
2014-11-30 04:44:46 -0700

    change hours and minutes to milliseconds + and moved ms converters to helper

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -13,32 +13,14 @@ class Subscription < ActiveRecord::Base
     self.company.stripe_customer_object.subscriptions.data.first.plan[:id][0,2].to_i
   end
 
-  def hours_used(since, ended)
-    ms = TogglRequester.new(:client_name => self.company.name, :since => since, :until => ended).time
-    ms == nil ? 0 : milliseconds_to_hours(ms) 
-  end
-
-  def minutes_used(since, ended)
-    ms = TogglRequester.new(:client_name => self.company.name, :since => since, :until => ended).time
-    ms == nil ? 0 : milliseconds_to_minutes(ms) 
-  end
-
-  def hours_remaining(since, ended)
-    size - hours_used(since, ended)
+  def milliseconds_used(since, ended)
+    TogglRequester.new(:client_name => self.company.name, :since => since, :until => ended).time
   end
 
   private
 
   def stripe_subscription
     self.company.stripe_customer_object.subscriptions.data.first
-  end
-
-  def milliseconds_to_hours(ms)
-    (ms / 3600000) % 24
-  end
-
-  def milliseconds_to_minutes(ms)
-    (ms / 60000) % 60
   end
 
 end
